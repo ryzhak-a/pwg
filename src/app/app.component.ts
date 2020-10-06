f64f1f99a9ce885265a8ca4b4fcf72220a4fbc92
Andrii Ryzhak <55483532+ryzhak-andrii@users.noreply.github.com>
2020-10-06 13:36:47 +0300

    initial commit
    Completed project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwg';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string): void {
+    const parsedValue = parseInt(value, 10);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+
+  }
+
+  onChangeUseLetters(): void {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(): void {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(): void {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick(): void {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
